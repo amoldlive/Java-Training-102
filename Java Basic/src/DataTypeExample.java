cb41e760ff76c3ec83c916144c69919f45f88127
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-07-30 08:40:00 +0530

    intial commit
    intial commit

---
+++ Java Basic/src/DataTypeExample.java
@@ -0,0 +1,33 @@
+
+public class DataTypeExample {
+	public static void main(String[] args) {
+		
+		String s1="I am XYZ";
+		
+		byte b=1;
+		short s=10;
+		int i=10;
+		long l=100L;
+		
+		float f=10.10F;
+		double d=10.30D;
+		
+		boolean b1=true;
+		boolean b2=false;
+		
+		char ch='A';
+		
+		System.out.println("String Value : "+s1);
+		System.out.println("byte Value : "+b);
+		System.out.println("short Value : "+s);
+		System.out.println("int Value : "+i);
+		System.out.println("long Value : "+l);
+		System.out.println("float Value : "+f);
+		System.out.println("double Value : "+d);
+		System.out.println("boolean Value : "+b1);
+		System.out.println("boolean Value : "+b2);
+		System.out.println("char Value : "+ch);
+		
+		
+	}
+}
---
+++ Java Basic/src/MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+
+public class MyFirstJavaProgram {
+	public static void main(String[] args) {
+		String name="XYZ";
+		System.out.println("Hello I am "+name);
+	}
+}
