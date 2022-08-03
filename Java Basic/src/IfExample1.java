fa97d6d09289f415e4529c13403b0a6a2985f244
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-08-04 04:58:15 +0530

    if else
    if else

---
+++ Java Basic/src/IfExample1.java
@@ -0,0 +1,19 @@
+
+public class IfExample1 {
+	public static void main(String[] args) {
+		/* find max number */
+		int num1 = 1;
+		int num2 = 10;
+
+		if(num1>num2) {
+			System.out.println("Max Number : "+num1);
+		}else {
+			System.out.println("Else-Max Number : "+num2);
+		}
+		
+		if(num1>0) {
+			System.out.println("NUmber is greater than 0");
+		}
+		/* find min number :Assignment*/
+	}
+}
---
+++ Java Basic/src/IfExample2.java
@@ -0,0 +1,24 @@
+
+public class IfExample2 {
+	public static void main(String[] args) {
+		/*number +ve , number <100 , number -ve , number >-100 */
+		
+		int num=90;
+		if(num>0) {
+			System.out.println("Number is +ve");
+			if(num<100) {
+				System.out.println("Number is less than 100");
+			}else {
+				System.out.println("Number is greater than 100");
+			}
+		}else {
+			System.out.println("Number is -ve");
+			if(num>-100) {
+				System.out.println("Number is greater than -100");
+			}else {
+				System.out.println("Number is less than -100");
+			}
+		}
+
+	}
+}
