9d358603a68a79917c8c867462482b91a71411c8
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-08-18 11:22:11 +0530

    Arrays Class
    Arrays Class

--- Java Basic/src/com/myapp/basic/ArrayExample7.java
+++ Java Basic/src/com/myapp/basic/ArrayExample7.java
@@ -1,31 +1,46 @@
 package com.myapp.basic;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 /*Arrays Class*/
 public class ArrayExample7 {
 
 	public static void main(String[] args) {
-		
-		//array 1
-		int elementArr[]= {101,45,11,21,31,81,91,51}; //unsorted Array
-		int searchElement=31;
-			
-	
-		
+
+		// array 1
+		int elementArr[] = { 101, 45, 11, 21, 31, 81, 91, 51 }; // unsorted Array
+
+		for (int i = 0; i < elementArr.length; i++) {
+			System.out.print(elementArr[i] + "\t");
+		}
+
 		Arrays.sort(elementArr);
-		
-		
-		int location=Arrays.binarySearch(elementArr, searchElement);
-		
-		System.out.println("Element : "+searchElement +" |  Location of element : "+location);
-	
-		int[] arr1= {10,20,30};
-		int[] arr2= {10,20,30};
-		int res=Arrays.compare(arr1, arr2);
-		System.out.println("Compare Result : "+res);
+		System.out.println();
+
+		for (int i = 0; i < elementArr.length; i++) {
+			System.out.print(elementArr[i] + "\t");
+		}
+
+		System.out.println("\n*******************************");
+		int search = 21;
+		int location = Arrays.binarySearch(elementArr, search);
+		if (location < 0) {
+			System.out.println("Element not found");
+		} else {
+
+			System.out.println("Element : " + search + " |  Location of element : " + location);
+		}
+
+		System.out.println("\n***********************************");
+
+		int[] arr1 = { 10, 20, 30 };
+		int[] arr2 = { 10, 20, 30 };
+		int res = Arrays.compare(arr1, arr2);
+		//res == 0 : matched
+		//res <0 : unmatched
+		System.out.println("Compare Result : " + res);
+
 	}
-	
-	
-	
+
 }
