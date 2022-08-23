9d4ef670cc5bb2578e7c0e54d1815b6d52bba36d
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-08-23 08:04:07 +0530

    updated files
    updated files

--- Java Basic/src/com/myapp/oop/A_Class.java
+++ Java Basic/src/com/myapp/oop/A_Class.java
@@ -2,7 +2,58 @@ package com.myapp.oop;
 
 // blueprint of the object
 // Naming convention - First Letter should be caps and it should be character
-// 
+class First {
+	// variable : instance | local | static
+	// instance variable
+	int i = 10;
+	String name = "softronix";
+	boolean isPersent = true;
+
+	// method : instance | static
+
+	// instance method
+	// public -> access modifier
+	// void -> return type /data type
+	// show -> method name
+	public void show() {
+		// some logic
+		System.out.println("show->"+i);
+		System.out.println("show->"+name);
+		System.out.println("show->"+isPersent);
+	}
+
+	// parameters
+	public void add(int a, int b) {
+		// some logic
+		int result=a+b;
+		System.out.println("Result of Addition: "+result);
+	}
+
+	// with return
+	public int sub(int a, int b) {
+		// some logic
+		int result=a-b;
+		System.out.println("substraction result "+result);
+		
+		return result;
+	}
+
+	// with return
+	public String getName(String name) {
+		// some logic
+		return name;
+	}
+
+}
+
+class Second {
+
+}
+
+class Third {
+
+}
+
 public class A_Class {
 
 	// constructor
@@ -11,9 +62,17 @@ public class A_Class {
 
 	// some methods
 
-	//block
-	
+	// block
+
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		First firstObject = new First(); // object --instance of a class
+		System.out.println(firstObject.i);
+		System.out.println(firstObject.name);
+		System.out.println(firstObject.isPersent);
+		firstObject.show();
+		firstObject.add(10, 20);
+		
+		int result=firstObject.sub(20, 10);
+		System.out.println("in mian - substractionr result : "+result);
 	}
 }
---
+++ Java Basic/src/com/myapp/oop/CalculatorMain.java
@@ -0,0 +1,45 @@
+package com.myapp.oop;
+
+class Calculator {
+
+	public void add(int a, int b) {
+		int add=a+b;
+		System.out.println("Addition : "+add);
+	}
+
+	public void sub(int a, int b) {
+
+	}
+
+	public void mul(int a, int b) {
+
+	}
+
+	public void div(int a, int b) {
+
+	}
+
+}
+class Calculator1{
+	public int add(int a, int b) {
+		return a+b;
+	}
+
+	public int sub(int a, int b) {
+		return 0;
+	}
+
+	public int mul(int a, int b) {
+		return 0;
+	}
+
+	public double div(int a, int b) {
+		return 0.0;
+	}
+	
+}
+public class CalculatorMain {
+	public static void main(String[] args) {
+			
+	}
+}
