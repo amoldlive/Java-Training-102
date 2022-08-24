2031a666bbfab8dcad632eb98a3af40459a21140
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-08-24 10:43:38 +0530

    changes updated
    changes updated

--- Java Basic/src/com/myapp/oop/AlphabetPrintingMachine.java
+++ Java Basic/src/com/myapp/oop/AlphabetPrintingMachine.java
@@ -1,5 +1,22 @@
 package com.myapp.oop;
 
-public class AlphabetPrintingMachine {
+class AlphabetMould {
 
+	String ink = "Black";
+
+	public void print(String printerName,String inputInk) {
+		System.out.println("Printer Name : "+printerName +" Ink Used : " + inputInk);
+
+		for (int i = 65; i < 91; i++) {
+			System.out.print((char) i + "\t");
+		}
+	}
+
+}
+
+public class AlphabetPrintingMachine {
+	public static void main(String[] args) {
+		AlphabetMould alphabetMould = new AlphabetMould();
+		alphabetMould.print("Canon-101","Yellow");
+	}
 }
--- Java Basic/src/com/myapp/oop/MarksheetCalculationSystem.java
+++ Java Basic/src/com/myapp/oop/MarksheetCalculationSystem.java
@@ -1,5 +1,33 @@
 package com.myapp.oop;
 
-public class MarksheetCalculationSystem {
+class MarksheetCalculator{
 
+	int mark1=45; //out of 100
+	int mark2=65; //out of 100
+	int mark3=85; //out of 100
+	
+	public int calcTotal() {
+		int total =mark1+mark2+mark3;
+		System.out.println("Total Marks : "+total);
+		return total;
+	}
+	public void calcAvrage(int total) {
+		double avg =total/3;
+		System.out.println("Avergae Marks : "+avg);
+	}
+	public void calcPercentage(int total) {
+		double percentage =(total*100)/300;
+		System.out.println("Percentage : "+percentage +"%");
+	}
+	
+	
+}
+public class MarksheetCalculationSystem {
+	public static void main(String[] args) {
+		MarksheetCalculator calculator=new MarksheetCalculator();
+		
+		int total=calculator.calcTotal();
+		calculator.calcAvrage(total);
+		calculator.calcPercentage(total);
+	}
 }
