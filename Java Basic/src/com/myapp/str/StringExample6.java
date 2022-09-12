f8d1415d862c7fd0bf6defd125ab19ba6c75d991
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-09-12 08:02:59 +0530

    Resource
    Resource

---
+++ Java Basic/src/com/myapp/str/StringExample6.java
@@ -0,0 +1,15 @@
+package com.myapp.str;
+
+//reverse the string
+public class StringExample6 {
+	public static void main(String[] args) {
+
+		// Heap Memory
+		String str = new String("Hello");
+		String str2 = new String("Hello");
+
+		
+		String str3=str.intern(); // point to the object created in SCP
+	}
+
+}
